d618dffe1b60eba048c9685ae574b91e29e7bb6f
Irving Henriquez <henriquezirv@gmail.com>
2021-05-18 00:32:50 +0000

    added timeago as a dependency

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,9 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+$(document).ready( function(){
+  
+    // Once we fetch tweet data and render tweets
+      // we can access "created at" property and feed value to timeago?
+    // $(".tweet-timestamp").text(timeago.format(time))
+})
