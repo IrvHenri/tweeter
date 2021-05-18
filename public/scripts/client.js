44d93bd5058c369f6c6c0f2c6b6c65ee0c53911a
Irving Henriquez <henriquezirv@gmail.com>
2021-05-18 17:56:19 +0000

    added fixed nav styling and formatted project files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,5 +7,6 @@ $(document).ready( function(){
   
     // Once we fetch tweet data and render tweets
       // we can access "created at" property and feed value to timeago?
-    // $(".tweet-timestamp").text(timeago.format(time))
+
+      $(".tweet-timestamp").text(timeago.format(1621182256239))
 })
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,15 @@
 $(document).ready(function () {
- 
-  
-  function charCounter(){
+  function charCounter() {
     let charLimit = 140;
     let charCount = this.value.length;
     //Instead of explicitly find .counter we can traverse DOM tree to parent & find node sibling
-    let counter = $(this).parent().find('.counter')
+    let counter = $(this).parent().find(".counter");
     let charactersRemaining = charLimit - charCount;
     if (charactersRemaining < 0) {
       $(counter).css("color", "red");
     }
     $(counter).text(charactersRemaining);
   }
-  
-  $("#tweet-text").on("input",charCounter);
+
+  $("#tweet-text").on("input", charCounter);
 });
